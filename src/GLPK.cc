eda91bcb8906d07aac6d85a976bba9ea4762dd13
Arthur <arthrfrnco@gmail.com>
2020-08-02 21:28:57 -0300

    Merge description (readme, license etc) updates (#3)
    * update README
    
    * update README, tests and Options.cc
    
    * update License, TODO, README

--- src/GLPK.cc
+++ src/GLPK.cc
@@ -1,10 +1,31 @@
-/* Normally all GLPK API routines start array indexing from 1, not from 0 (except the specially stipulated cases). 
+/*
+ * Copyright 2020 Arthur Moreira
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * opw is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
-This means, for example, that if some vector x of the length n is passed as an array to some GLPK API routine, the latter expects vector components to be placed in locations x[1], x[2], . . . , x[n], and the location x[0] normally is not used.
 
-To avoid indexing errors it is most convenient and most reliable to declare the array x as follows:
-
-    double x[1+n];
+/* Normally all GLPK API routines start array indexing from 1,
+ * not from 0 (except the specially stipulated cases). 
+ * This means, for example, that if some vector x of the length n
+ * is passed as an array to some GLPK API routine, the latter expects 
+ * vector components to be placed in locations x[1], x[2], . . . , x[n], 
+ * and the location x[0] normally is not used.
+ * To avoid indexing errors it is most convenient and most reliable
+ * to declare the array x as follows:
+ * double x[1+n];
+ *
 */
 
 
