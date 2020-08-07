5f10ad66e0a29cc8ae627a6b6201a80e96119bd6
a-moreira <arthrfrnco@gmail.com>
2020-08-07 14:47:00 -0300

    update License
    Create LICENSE.md
    
    Merge branch "teste", with License updates  (#4)
    
    * update README
    
    * update README, tests and Options.cc
    
    * update License, TODO, README
    
    update License in some files

--- src/ModelGAP.cc
+++ src/ModelGAP.cc
@@ -1,5 +1,6 @@
 /*
- * Copyright 2020 Arthur Moreira
+ * Copyright 2020
+ *  Arthur Moreira
  *
  * This is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +13,6 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  */
 
 #include "ModelGAP.h"
